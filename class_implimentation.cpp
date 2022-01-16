a98e948f880f9e338b2597d5cea12c1235d3c82d
SujanSapkota07 <sujeansapkota47@gmail.com>
2022-01-16 16:05:14 +0545

    book ma bhako program copy gareko ho aru kei hoena

---
+++ class_implimentation.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+class item
+{
+    int number;
+    float cost;
+public:
+    void getdata(int a, float b);
+    void putdata(void)
+    {
+        cout << "number= " << number << "\n";
+        cout << "cost= " << cost << "\n";
+    }
+};
+
+void item :: getdata(int a, float b)
+{
+    number = a;
+    cost = b;
+}
+
+int main()
+{
+    item x;
+    cout << "\nObject x \n";
+    x.getdata(100, 75.5);
+
+    x.putdata();
+    item y;
+    cout << "\nObject y \n";
+    y.getdata(101, 1000);
+
+    y.putdata();
+
+
+    return 0;
+}
+
+/* ERROR IN THIS PROGRAM
+
+
+
+
+*/
+
+
+
